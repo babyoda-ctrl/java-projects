53ca7aa20c6882d0488e2b038ca4de9448b1cbdd
babyoda-ctrl <onughacharles11@gmail.com>
2026-01-25 00:47:19 +0100

    Add files via upload

---
+++ AudioPlayer.java
@@ -0,0 +1,58 @@
+import javax.sound.sampled.*;
+import java.io.*;
+import java.util.Scanner;
+
+
+//AudioPlayer in java 😁
+public class Main{
+    public static void main(String[] args) throws IOException {
+        String filePath ="C:\\Users\\TOSHIBA\\Downloads\\24kGoldn - Company (Official Video) ft. Future.wav";
+        File file = new File(filePath);
+
+
+        try(Scanner scanner = new Scanner(System.in);
+            AudioInputStream audioStream = AudioSystem.getAudioInputStream(file)){
+
+
+            Clip clip = AudioSystem.getClip();
+            clip.open(audioStream);
+            clip.start();
+
+            String response = "";
+
+            while(!response.equals("Q")){
+                System.out.println("P = Play");
+                System.out.println("S = Stop");
+                System.out.println("R = Reset");
+                System.out.println("Q = Quit");
+                System.out.println("Enter your choice: ");
+
+                response = scanner.next().toUpperCase();
+
+                switch (response){
+                    case "P" -> clip.start();
+                    case "S" -> clip.stop();
+                    case "R" -> clip.setMicrosecondPosition(0);
+                    case "Q" -> clip.close();
+                    default -> System.out.println("Invalid Choice");
+                }
+
+            }
+        }
+        catch(FileNotFoundException e){
+            System.out.println("Could not locate file");
+        }
+        catch(UnsupportedAudioFileException e){
+            System.out.println("Audio file is not supported");
+        }
+        catch(LineUnavailableException e){
+            System.out.println("Unable to access audio resource");
+        }
+        catch(IOException e) {
+            System.out.println("Something went wrong");
+        }
+        finally{
+            System.out.println("Bye!");
+        }
+    }
+}
